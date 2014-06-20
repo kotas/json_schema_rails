291db71414c27f084a4c0f7be8f145a60416d7e6
kotas <kotas@kotas.jp>
2014-06-21 03:32:06 +0900

    Fix routing

--- spec/dummy_apps/rails4/config/routes.rb
+++ spec/dummy_apps/rails4/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:create]
+  resources :posts, only: [:create, :update]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
